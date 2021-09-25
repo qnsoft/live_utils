6e4aaf924d5147ce1d4bf8ceb5dd28b204e7283d
sxh <dfsxh@163.com>
2021-09-25 14:25:37 +0800

    修改命名空间

--- codec/h265.go
+++ codec/h265.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"errors"
 
-	"github.com/qnsoft/live_utils"
 	"github.com/q191201771/naza/pkg/nazabits"
+	"github.com/qnsoft/live_utils"
 )
 
 const (
@@ -84,7 +84,7 @@ const (
 )
 
 var AudNalu = []byte{0x00, 0x00, 0x00, 0x01, 0x46, 0x01, 0x10}
-var ErrHevc = errors.New("m7s.hevc: fxxk")
+var ErrHevc = errors.New("livev.hevc: fxxk")
 
 //HVCC
 type HVCDecoderConfigurationRecord struct {
